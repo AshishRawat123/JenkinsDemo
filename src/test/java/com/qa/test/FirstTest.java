4516d02ff7a4183ff5ac60c17255633d8e77e62d
Ashish Rawat <arawat@paymentus.com>
2020-10-13 01:58:54 +0530

    First and last jenkins commit

---
+++ src/test/java/com/qa/test/FirstTest.java
@@ -0,0 +1,16 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	@Test
+	public void first() {
+		System.out.println("First Class First Method");
+	}
+	
+	@Test
+	public void second() {
+		System.out.println("First Class Second Method");
+	}
+}
---
+++ src/test/java/com/qa/test/SecondTest.java
@@ -0,0 +1,18 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+	@Test
+	public void first() {
+		System.out.println("Second Class First Method");
+	}
+	
+	@Test
+	public void second() {
+		System.out.println("Second Class Second Method");
+	}
+	
+	
+}
